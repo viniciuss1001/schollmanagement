075189cadc7b67970893ddd7a404d3067b5aa545
Vinicius Frota <viniciussantosfrota10012017@gmail.com>
2025-02-04 20:34:50 -0300

    menu update

--- src/app/components/Menu.tsx
+++ src/app/components/Menu.tsx
@@ -5,18 +5,18 @@ import Image from "next/image"
 const MenuComponent = () => {
   return (
     <div className="mt-4 text-sm">
-      {menuItems.map((item)=>(
+      {menuItems.map((item) => (
         <div key={item.title} className="flex flex-col gap-2">
-          <span className="hidden lg:block text-gray-400 font-light my-4">
+          <span className="hidden lg:block font-light my-4">
             {item.title}
           </span>
-          {item.items.map((itemlink)=> (
-            <Link key={itemlink.label} href={itemlink.href}
-            className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 p-2 hover: hover:animate-pulse rounded-md"
-            >
-              <Image alt="" src={itemlink.icon} width={20} height={20}/>
-              <span className="hidden lg:block">{itemlink.label}</span>
-            </Link>
+          {item.items.map((itemlink) => (
+              <Link key={itemlink.label} href={itemlink.href}
+                className="flex justify-center items-center lg:justify-start gap-4 p-2 hover: hover:animate-pulse rounded-md"
+              >
+                <Image alt="" src={itemlink.icon} width={20} height={20} />
+                <span className="hidden lg:block">{itemlink.label}</span>
+              </Link>
           ))}
         </div>
       ))}
