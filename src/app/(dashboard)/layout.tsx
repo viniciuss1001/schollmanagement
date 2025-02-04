e308dd07836672de6cbf02ba4026dbef2d4a100a
Vinicius Frota <viniciussantosfrota10012017@gmail.com>
2025-02-04 12:55:39 -0300

    Installing dark an ligth mode

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
                 </Link>
                 <MenuComponent />
             </div>
-            <div className="w-[86%] md:w-[92%%] lg:w-[84%] xl:w-[86%] bg-[#f7f8fa] overflow-scroll">
+            <div className="w-[86%] md:w-[92%%] lg:w-[84%] xl:w-[86%] overflow-scroll">
                 <NavbarComponent />
                 {children}
             </div>
--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,14 +1,16 @@
 import Image from 'next/image'
 import React from 'react'
+import { ModeToggle } from './Theme-Toggle'
 
 const NavbarComponent = () => {
   return (
     <div className='flex items-center justify-between p-4'>
       <div className='hidden md:flex items-center gap-2 text-xs rounded-full ring-[1.5px] ring-gray-300 px-2'>
         <Image src='/search.png' alt='' width={14} height={14} />
-        <input placeholder='Pesquisar' className='w-[200px] p-2 bg-transparent outline-none'/>
+        <input placeholder='Pesquisar' className='w-[200px] p-2 bg-transparent outline-none' />
       </div>
-      <div className='flex items-center gap-6'>
+      <div className='flex items-center gap-6 justify-end w-full'>
+        <ModeToggle />
         <div className='bg-white rounded-full w-7 h-7 flex items-center justify-center cursor-pointer'>
           <Image src='/message.png' alt='' width={20} height={20} />
         </div>
@@ -20,8 +22,8 @@ const NavbarComponent = () => {
           <span className='text-xs leading-3 font-medium '>Fulano</span>
           <span className='text-[10px] text-gray-500 text-right'>Admin</span>
         </div>
-        <Image src='/avatar.png' alt='User' height={36} width={36} 
-        className='rounded-full'
+        <Image src='/avatar.png' alt='User' height={36} width={36}
+          className='rounded-full'
         />
       </div>
     </div>
